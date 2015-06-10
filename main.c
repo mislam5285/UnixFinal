3fe0da30a580aa0f0ebb2e37368137fd1ad359a7
Khoa Nguyen <n_nhukhoa@yahoo.com.vn>
2015-06-11 02:22:41 +0700

    Executable but still buggy

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 int checkValidCommand(char **command, char *cmd[]){
 	int i;
 	int match =0;
-	for (i = 0; i < 6; i++){
+	for (i = 0; i < 7; i++){
 		if(*cmd[i] == **command){ 
 			match =1;
 			break;
@@ -33,6 +33,20 @@ void cutCommand(char *commandline, char **argv){
 	*argv = '\0';
 }
 
+void execute(char **argv){
+	pid_t pid;
+	int status;
+	if((pid=fork()) < 0){
+		printf("ERROR: FORK CHILD PROCESS FAILED! \n");
+		exit(EXIT_FAILURE);
+	} else if (pid == 0){
+		execvp(argv[0], argv);
+		printf("EXECUTE COMMAND FAILED! \n");
+	} else{
+		while (wait(&status) != pid);
+	}
+}
+
 int main(){
 	char commandline[1024];
 	char *cmd[7] = {"cd", "cp", "ls", "date", "who", "cat", "exit"};
@@ -47,11 +61,13 @@ int main(){
 		match =0;
 		match = checkValidCommand(argv, cmd);
 		if (match == 1){
-			printf("Valid Command! \n");
+			printf("\n");
 		} else{
 			printf("Invalid Command! \n");
 			continue;
 		}
+		
+		execute(argv);
 			
 	}
 	
